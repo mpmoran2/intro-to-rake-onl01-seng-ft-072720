af56497a6d764ab0cdba1709c078a50aa6d7c364
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-29 15:56:24 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,3 @@ require_relative "../lib/student.rb"
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
 
-task :environment do
-  require_relative './config/environment'
-end
-
-desc 'drop into the Pry console'
-task :console => :environment do
-  Pry.start
-end
